caff51727d0248c61385f3c24d98e9b5fd355749
Austin Atmaja <austin.atmaja@gmail.com>
2018-12-01 01:15:08 -0500

    add google sign in code

--- server/main.js
+++ server/main.js
@@ -3,6 +3,59 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+var fs = require("fs");
+
+const {google} = require('googleapis')
+
+var content = fs.readFileSync("../credentials.json");
+var credentialsJson = JSON.parse(content);
+
+const googleConfig = {
+    clientId: credentialsJson.installed.client_id, // e.g. asdfghjkljhgfdsghjk.apps.googleusercontent.com
+    clientSecret: credentialsJson.installed.client_secret, // e.g. _ASDFA%DFASDFASDFASD#FAD-
+    redirect: credentialsJson.installed.redirect_uris[1] // this must match your google api settings
+};
+
+/**
+ * Create the google auth object which gives us access to talk to google's apis.
+ */
+function createConnection() {
+    return new google.auth.OAuth2(
+        googleConfig.clientId,
+        googleConfig.clientSecret,
+        googleConfig.redirect
+    );
+}
+
+
+/**
+ * This scope tells google what information we want to request.
+ */
+const defaultScope = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/userinfo.email',
+];
+
+/**
+ * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
+ */
+function getConnectionUrl(auth) {
+    return auth.generateAuthUrl({
+        access_type: 'offline',
+        prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
+        scope: defaultScope
+    });
+}
+
+  /**
+ * Create the google url to be sent to the client.
+ */
+function urlGoogle() {
+    const auth = createConnection(); // this is from previous step
+    const url = getConnectionUrl(auth);
+    return url;
+}
+
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
@@ -11,6 +64,11 @@ app.use(function(req, res, next) {
 
 app.get('/api/test', (req, res) => {
   res.send({ "hi": 'Hello From Express' });
+});
+
+app.get('/api/signIn', (req, res) => {
+    const url = urlGoogle();
+    res.send({'url': url});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
